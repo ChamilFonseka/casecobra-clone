6e025e56603701470a0fc307c57a714892d474a1
Chamil Fonseka <fonseka.chamil@gmail.com>
2025-02-23 20:54:04 +0530

    added customize design step

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["ksygxi9rcw.ufs.sh"],
+  },
 };
 
 export default nextConfig;
